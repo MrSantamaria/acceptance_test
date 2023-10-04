ec534378d7290d3d6322ecccf2d3cf7d1655fdce
Diego Santamaria <dsantama@redhat.com>
2023-10-04 17:21:21 -0600

    Trial SSS code

--- main.go
+++ main.go
@@ -4,31 +4,60 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/openshift/hive/contrib/pkg/utils"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/openshift/hive/apis"
+	v1 "github.com/openshift/hive/apis/hive/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func main() {
-	// Create a new context.
-	ctx := context.TODO()
+	//kubeconfig := "/Users/dsantama/Documents/GitHub/acceptance_test/kubeconfig"
+	// Create a new scheme and add necessary APIs to it.
+	scheme := runtime.NewScheme()
+	if err := apis.AddToScheme(scheme); err != nil {
+		fmt.Printf("Error adding APIs to scheme: %v\n", err)
+		return
+	}
 
-	client, err := utils.GetClient()
+	// Get the Kubernetes client using in-cluster configuration.
+	customClient, err := GetClient(scheme) // Pass the scheme to GetClient function
 	if err != nil {
 		fmt.Printf("Error creating dynamic client: %v\n", err)
 		return
 	}
 
-	// Use this client to list all namespaces.
-	nsList, err := client.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
+	sss := &v1.SelectorSyncSet{}
+
+	err = customClient.Get(context.TODO(), client.ObjectKey{Namespace: "cluster-scope", Name: "aws-vpce-operator-hypershift-sss-us-west-2-main"}, sss)
 	if err != nil {
-		fmt.Printf("Error listing namespaces: %v\n", err)
-		return
+		fmt.Println(err)
 	}
 
-	// Print the list of namespaces.
-	fmt.Printf("Namespaces:\n")
-	for _, ns := range nsList.Items {
-		fmt.Printf("  %s\n", ns.Name)
+	fmt.Println(sss)
+
+}
+
+// GetClient returns a new dynamic controller-runtime client.
+func GetClient(scheme *runtime.Scheme) (client.Client, error) {
+	cfg, err := GetClientConfig()
+	if err != nil {
+		return nil, err
 	}
 
+	// Create a dynamic controller-runtime client.
+	dynamicClient, err := client.New(cfg, client.Options{Scheme: scheme})
+	if err != nil {
+		return nil, err
+	}
+
+	return dynamicClient, nil
+}
+
+// GetClientConfig gets the config for the REST client.
+func GetClientConfig() (*restclient.Config, error) {
+	rules := clientcmd.NewDefaultClientConfigLoadingRules()
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, &clientcmd.ConfigOverrides{})
+	return kubeconfig.ClientConfig()
 }
